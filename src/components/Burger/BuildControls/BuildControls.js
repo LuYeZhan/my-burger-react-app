d6e4726ceca665a5a5beca2cf22aa6cc6e9fd28b
Ye Zhan <lu@hooli.store>
2020-06-17 22:50:49 +0200

    connecting state to build control adding ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -5,14 +5,20 @@ import BuildControl from './BuildControl/BuildControl';
 const controls = [
   { label: 'Salad', type: 'salad' },
   { label: 'Bacon', type: 'bacon' },
-  { label: 'Chesse', type: 'chesse' },
+  { label: 'Cheese', type: 'cheese' },
   { label: 'Meat', type: 'meat' },
 ];
 
 const buildcontrols = (props) => (
   <div className={classes.BuildControls}>
     {controls.map((ctrl) => {
-      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+      return (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+        />
+      );
     })}
   </div>
 );
