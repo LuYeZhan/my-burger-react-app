c06f1da2fb6fdc2a7de77d15b6cdd5ca19a3016e
Ye Zhan <lu@hooli.store>
2020-06-17 19:19:38 +0200

    outputting multiple build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildcontrols = (props) => <div>pepe</div>;
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Chesse', type: 'chesse' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildcontrols = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />;
+    })}
+  </div>
+);
 
 export default buildcontrols;
